f28e408e321bf765a3c4effb67e6b7a9a486445b
Markus Pfeifer <mkspfeifer@gmail.com>
2023-07-17 00:20:23 +0200

    added json and logging

--- communication.py
+++ communication.py
@@ -1,12 +1,15 @@
 # /usr/bin/env python
-
+import logging
 import serial
 import datetime
 import time
 import os
+import json
 
 
 def main():
+    logging.basicConfig(filename="communication.log", level=logging.INFO, format='%(asctime)s %(message)s')
+
     with serial.Serial('/dev/ttyACM0', 115200) as connection:
         time.sleep(3)
         while True:
@@ -14,29 +17,38 @@ def main():
             if connection.readline() == b"connection confirmation\r\n":
                 connection.write(b"connection acknowledgement\n")
                 break
-        print("Established connection (3-way handshake).")
+        logging.info("Established connection (3-way handshake).")
         if not (data_directory := "Data") in os.listdir():
             os.mkdir(data_directory)
-            print("Created 'Data' directory.")
+            logging.info("Created 'Data' directory.")
         else:
-            print("Checked if 'Data' directory is available.")
-        # TODO: Add a separate json file which holds all additional data for the session.
-        # TODO: Create directory for each Session.
+            logging.info("Checked if 'Data' directory is available.")
         # TODO: Add the possibility to stop an ongoing session and continue it afterwards.
-        new_filename = f"Data/Distances_{datetime.datetime.now().strftime('%D_%H_%M_%S').replace('/', '_')}.csv"
-        connection.write(f"{250.}\n".encode())  # x distance
-        connection.write(f"{80.}\n".encode())  # y distance
-        connection.write(f"{1.}\n".encode())  # x distance step
-        connection.write(f"{1.}\n".encode())  # y distance step
+        new_filename = f"Data/Distances_{datetime.datetime.now().strftime('%D_%H%M%S').replace('/', '')}.csv"
+        configuration = {
+            "x distance [mm]": float(input("Enter the x distance [mm]: ")),
+            "y distance [mm]": float(input("Enter the y distance [mm]: ")),
+            "x distance_step [mm]": float(input("Enter the x distance step [mm]: ")),
+            "y distance_step [mm]": float(input("Enter the y distance step [mm]: ")),
+        }
+        for setting, value in configuration.items():
+            connection.write(f"{value}\n".encode())
+            logging.info(f"Sent '{setting}': {value}.")
+        configuration["sphere diameter [mm]"] = float(input("Enter the diameter of the sphere [mm]: "))
+        configuration["top tripod [mm]"] = float(input("Enter the height of the top tripod [mm]: "))
+        configuration["top sand [mm]"] = float(input("Enter the height of the top sand [mm]: "))
+        with open(new_filename.replace(".csv", ".json"), 'w') as json_file:
+            json.dump(configuration, json_file)
+
         with open(new_filename, 'a') as csv_file:
             csv_file.write("x [mm],y [mm],z [mm]\n")
         time.sleep(1)
         connection.write(b"__start__\n")
-        print("Sent start message.")
+        logging.debug("Sent start message.")
         time0 = datetime.datetime.now()
         while (message := connection.readline().decode().replace("\r", "")) != "__close__\n":
             time1 = datetime.datetime.now()
-            print(message.replace("\n", "") + f"\t{time1-time0}")
+            logging.debug(message.replace("\n", "") + f"\t{time1-time0}")
             time0 = time1
             with open(new_filename, 'a') as csv_file:
                 csv_file.write(message)
